63c4e6e04abe7b53b007bee32f19137525de304f
Janella Senica <janella.senica@gmail.com>
2020-08-19 22:15:41 -0500

    added/rev components

---
+++ reactportfolio/src/components/Projects/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class Gallery extends Component {
+    
+}
+const images = [
+    "../../public/assets/plants.jpg",
+    "../../public/assets/cooking.jpg",
+    "../../public/assets/fitness.jpg",
+    "../../public/assets/employee.jpg",
+    "../../public/assets/password.jpg",
+    "../../public/assets/budget.jpg",
+]
+
+class Projects extends Component {
+
+}
+
+
