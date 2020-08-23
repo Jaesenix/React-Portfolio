43758d6b074695fa8f69824f436859879bf14ada
Janella Senica <janella.senica@gmail.com>
2020-08-23 10:51:56 -0500

    Updates

---
+++ precache-manifest.02796933bb5e5c4d020d54cadadb85ac.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6dc0fa45226809b9f702d37e1e47fff",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "1b2224fe2fc2558d3cd6",
+    "url": "/React-Portfolio/static/css/main.54ac075a.chunk.css"
+  },
+  {
+    "revision": "a9a5b95ad1d01e80e9f3",
+    "url": "/React-Portfolio/static/js/2.27a63ef3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React-Portfolio/static/js/2.27a63ef3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b2224fe2fc2558d3cd6",
+    "url": "/React-Portfolio/static/js/main.04aec167.chunk.js"
+  },
+  {
+    "revision": "0274f599536680666069",
+    "url": "/React-Portfolio/static/js/runtime-main.27e0f504.js"
+  },
+  {
+    "revision": "8778ce090965505469fc8e04e6919d56",
+    "url": "/React-Portfolio/static/media/budget.8778ce09.png"
+  },
+  {
+    "revision": "cff5ffe96e23dd9a52d7d62d07835aae",
+    "url": "/React-Portfolio/static/media/coffee.cff5ffe9.png"
+  },
+  {
+    "revision": "f15a4213a6473f6808fa38e11a53aa49",
+    "url": "/React-Portfolio/static/media/cooking.f15a4213.png"
+  },
+  {
+    "revision": "4f921a408b352801d59651174575aa9b",
+    "url": "/React-Portfolio/static/media/employee.4f921a40.png"
+  },
+  {
+    "revision": "2ab1edc39cb3225613a498599d002d89",
+    "url": "/React-Portfolio/static/media/fitness.2ab1edc3.png"
+  },
+  {
+    "revision": "2fc901279080b6990f958b6ba8393211",
+    "url": "/React-Portfolio/static/media/password.2fc90127.png"
+  },
+  {
+    "revision": "04e2e0df1d9589c9edbc619febad5829",
+    "url": "/React-Portfolio/static/media/plants.04e2e0df.png"
+  }
+]);
